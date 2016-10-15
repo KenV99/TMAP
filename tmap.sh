6cc7caa982abcfa89847f2c4567962a9bb66a0b1
KenV99 <KenV99@users.noreply.github.com>
2016-10-15 14:33:43 -0400

    Prompt for openssh-server. Install ntp.

--- tmap.sh
+++ tmap.sh
@@ -10,15 +10,15 @@ function tmap-setup() {
    fi
    echo -e "${green}If you plan to run the VM as a service, openssh-server should be installed so that you can log in while it is running under the SYSTEM account."
    read -p  read -p "Install openssh-server? [Y/n]${reset}" OSSHResponse
+   if [ "$OSSHResponse" == "" ] || [ "$OSSHResponse" == "Y" ] || [ "$OSSHResponse" == "y" ]
+   then
+      sudo apt install -y openssh-server
+   fi
    sudo sed -i.$(date "+%m%d%y").bak "s/ubuntu/$MNAME/g" /etc/hosts
    sudo sed -i.$(date "+%m%d%y").bak "s/ubuntu/$MNAME/g" /etc/hostname
    sudo sed -i.$(date "+%m%d%y").bak '/^deb cdrom:/s/^/# /' /etc/apt/sources.list
    sudo dpkg-reconfigure tzdata
    sudo apt update
-   if [ "$OSSHResponse" == "" ] || [ "$OSSHResponse" == "Y" ] || [ "$OSSHResponse" == "y" ]
-   then
-      sudo apt install -y openssh-server
-   fi
    sudo apt install -y unattended-upgrades ntp
    sudo apt full-upgrade -y
    sudo sed -i.$(date "+%m%d%y").bak "$ a ~/TMAP/tmap.sh after-reboot" ~/.profile
